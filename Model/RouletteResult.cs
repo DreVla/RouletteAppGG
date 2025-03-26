217a75685af52f4d22819c987a23304baee8f6c5
Vlad Dreghici-Popa <32488956+DreVla@users.noreply.github.com>
2025-03-26 14:06:14 +0200

    Initial Commit

---
+++ Model/RouletteResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouletteApp.Model
+{
+    public class RouletteResult
+    {
+        public int position { get; set; }
+        public string color { get; set; }
+        public int streak { get; set; }
+        public int multiplier => position * streak;
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouletteApp.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _notificationMessage;
+        public string NotificationMessage
+        {
+            get => _notificationMessage;
+            set
+            {
+                _notificationMessage = value;
+                OnPropertyChanged(nameof(NotificationMessage));
+            }
+        }
+
+        public ObservableCollection<string> Results { get; set; } = new();
+
+        public MainViewModel()
+        {
+            NotificationMessage = "Welcome to the Roulette App!";
+        }
+    }
+}
